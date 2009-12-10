0a217b72486038d625220f017f06331c9fe713ee
rick <technoweenie@gmail.com>
2009-12-10 05:50:11 -0800

    paranoid about thread safety

--- lib/faraday.rb
+++ lib/faraday.rb
@@ -1,3 +1,11 @@
 module Faraday
+  # Loads each autoloaded constant.  If thread safety is a concern, wrap
+  # this in a Mutex.
+  def self.load
+    constants.each do |const|
+      const_get(const) if autoload?(const)
+    end
+  end
+
   autoload :Connection, 'faraday/connection'
 end
