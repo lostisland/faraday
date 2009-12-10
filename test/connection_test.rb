4930276c63e6029f7007a24e5455a835121dd328
rick <technoweenie@gmail.com>
2009-12-10 07:44:20 -0800

    using TestConnection in ConnectionTest is unnecessary

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -3,101 +3,101 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
 class ConnectionTest < Faraday::TestCase
   describe "#initialize" do
     it "parses @host out of given url" do
-      conn = TestConnection.new "http://sushi.com"
+      conn = Faraday::Connection.new "http://sushi.com"
       assert_equal 'sushi.com', conn.host
     end
 
     it "parses nil @port out of given url" do
-      conn = TestConnection.new "http://sushi.com"
+      conn = Faraday::Connection.new "http://sushi.com"
       assert_nil conn.port
     end
 
     it "parses @port out of given url" do
-      conn = TestConnection.new "http://sushi.com:815"
+      conn = Faraday::Connection.new "http://sushi.com:815"
       assert_equal 815, conn.port
     end
 
     it "parses nil @path_prefix out of given url" do
-      conn = TestConnection.new "http://sushi.com"
+      conn = Faraday::Connection.new "http://sushi.com"
       assert_equal '/', conn.path_prefix
     end
 
     it "parses @path_prefix out of given url" do
-      conn = TestConnection.new "http://sushi.com/fish"
+      conn = Faraday::Connection.new "http://sushi.com/fish"
       assert_equal '/fish', conn.path_prefix
     end
   end
 
   describe "#build_uri" do
     it "uses Connection#host as default URI host" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       conn.host = 'sushi.com'
       uri = conn.build_uri("/sake.html")
       assert_equal 'sushi.com', uri.host
     end
 
     it "uses Connection#port as default URI port" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       conn.port = 23
       uri = conn.build_uri("http://sushi.com")
       assert_equal 23, uri.port
     end
 
     it "uses Connection#path_prefix to customize the path" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       conn.path_prefix = '/fish'
       uri = conn.build_uri("sake.html")
       assert_equal '/fish/sake.html', uri.path
     end
 
     it "uses '/' Connection#path_prefix to customize the path" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       conn.path_prefix = '/'
       uri = conn.build_uri("sake.html")
       assert_equal '/sake.html', uri.path
     end
 
     it "forces Connection#path_prefix to be absolute" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       conn.path_prefix = 'fish'
       uri = conn.build_uri("sake.html")
       assert_equal '/fish/sake.html', uri.path
     end
 
     it "ignores Connection#path_prefix trailing slash" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       conn.path_prefix = '/fish/'
       uri = conn.build_uri("sake.html")
       assert_equal '/fish/sake.html', uri.path
     end
 
     it "allows absolute URI to ignore Connection#path_prefix" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       conn.path_prefix = '/fish'
       uri = conn.build_uri("/sake.html")
       assert_equal '/sake.html', uri.path
     end
 
     it "parses url/params into #path" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       uri = conn.build_uri("http://sushi.com/sake.html")
       assert_equal '/sake.html', uri.path
     end
 
     it "parses url/params into #query" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       uri = conn.build_uri("http://sushi.com/sake.html", 'a[b]' => '1 + 2')
       assert_equal "a%5Bb%5D=1%20+%202", uri.query
     end
 
     it "parses url into #host" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       uri = conn.build_uri("http://sushi.com/sake.html")
       assert_equal "sushi.com", uri.host
     end
 
     it "parses url into #port" do
-      conn = TestConnection.new
+      conn = Faraday::Connection.new
       uri = conn.build_uri("http://sushi.com/sake.html")
       assert_nil uri.port
     end
