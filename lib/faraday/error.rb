1e4fa3dab17abfcf2104eb9dbc686d59a0353ee2
rick <technoweenie@gmail.com>
2010-05-28 19:44:06 -0700

    Faraday::Error::ClientError is a common base class for all faraday errors

--- lib/faraday/error.rb
+++ lib/faraday/error.rb
@@ -1,6 +1,7 @@
 module Faraday
   module Error
-    class ConnectionFailed < StandardError; end
-    class ResourceNotFound < StandardError; end
+    class ClientError      < StandardError; end
+    class ConnectionFailed < ClientError;   end
+    class ResourceNotFound < ClientError;   end
   end
 end
