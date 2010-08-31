683146fba18bff91b01751f6512ceb92ca2e3024
Eric Lindvall <eric@5stops.com>
2010-08-31 13:01:58 -0700

    Use the correct hash for timeouts.

--- lib/faraday/adapter/typhoeus.rb
+++ lib/faraday/adapter/typhoeus.rb
@@ -23,8 +23,9 @@ module Faraday
           :headers => env[:request_headers],
           :disable_ssl_peer_verification => (env[:ssl][:verify] == false)
         
-        req.timeout = req.connect_timeout = (req[:timeout] * 1000) if req[:timeout]
-        req.connect_timeout = (req[:open_timeout] * 1000)          if req[:open_timeout]
+        env_req = env[:request]
+        req.timeout = req.connect_timeout = (env_req[:timeout] * 1000) if env_req[:timeout]
+        req.connect_timeout = (env_req[:open_timeout] * 1000)          if env_req[:open_timeout]
 
         req.on_complete do |resp|
           env.update \
