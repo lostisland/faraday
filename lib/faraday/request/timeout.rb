21bbebaf1c11f2f6e341dc7c9ca1fe3bde8ee7c3
Andrew Kane <andrew@contextoptional.com>
2011-10-06 12:27:01 -0700

    Use SystemTimer if possible in Ruby 1.8

--- lib/faraday/request/timeout.rb
+++ lib/faraday/request/timeout.rb
@@ -3,14 +3,29 @@ module Faraday
     dependency "timeout"
 
     def initialize(app, timeout = 2)
+      self.class.dependency "system_timer" if ruby18?
       @timeout = timeout
       super(app)
     end
 
     def call(env)
-      Timeout::timeout(@timeout) do
+      method =
+      if ruby18? && self.class.loaded?
+        SystemTimer.method(:timeout_after)
+      else
+        Timeout.method(:timeout)
+      end
+
+      method.call(@timeout) do
         @app.call(env)
       end
     end
+
+    private
+
+    def ruby18?
+      @ruby18 ||= RUBY_VERSION =~ /^1\.8/
+    end
+
   end
 end
