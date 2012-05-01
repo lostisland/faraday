4aa9492e4c8fed8cd1bb409f47cb97afc83f0559
Lars Gierth <lars@soundcloud.com>
2012-05-01 04:43:24 +0200

    Emit warning if skipping hatetepe tests

--- test/adapters/hatetepe_test.rb
+++ test/adapters/hatetepe_test.rb
@@ -1,12 +1,14 @@
 require File.expand_path("../integration", __FILE__)
 
 module Adapters
-  if defined?(Fiber)
-    class HatetepeTest < Faraday::TestCase
+  class HatetepeTest < Faraday::TestCase
 
-      def adapter() :hatetepe end
+    def adapter() :hatetepe end
 
+    if RUBY_VERSION >= "1.9"
       Integration.apply(self)
+    else
+      warn "Warning: Skipping Hatetepe tests in 1.8 mode"
     end
   end
 end
