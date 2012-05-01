0014a94ce1f4a90a7229b4cf59dbb11b1b099d74
Lars Gierth <lars@soundcloud.com>
2012-05-01 05:15:30 +0200

    Omit timeout test on JRuby

--- test/adapters/hatetepe_test.rb
+++ test/adapters/hatetepe_test.rb
@@ -6,7 +6,14 @@ module Adapters
     def adapter() :hatetepe end
 
     if RUBY_VERSION >= "1.9"
-      Integration.apply(self)
+      Integration.apply(self) do
+        if defined?(RUBY_ENGINE) && RUBY_ENGINE == "jruby"
+          # https://github.com/eventmachine/eventmachine/pull/289
+          # also EM::Connection#comm_inactivity_timeout is not
+          # implemented on jruby
+          undef :test_timeout
+        end
+      end
     else
       warn "Warning: Skipping Hatetepe tests in 1.8 mode"
     end
