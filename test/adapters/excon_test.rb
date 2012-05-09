d4f2a926cc81cd26f956383f1547b99ea6f23d13
Dan Dofter <dan@dofter.com>
2012-05-08 19:08:14 -0700

    set excon timeouts from faraday configuration and handle timeout errors

--- test/adapters/excon_test.rb
+++ test/adapters/excon_test.rb
@@ -10,9 +10,6 @@ module Adapters
       warn "Warning: Skipping Excon tests on Rubinius"
     else
       Integration.apply(self, :NonParallel) do
-        # https://github.com/eventmachine/eventmachine/pull/289
-        undef :test_timeout
-
         # FIXME: this test fails on Travis with
         # "Faraday::Error::ClientError: the server responded with status 400"
         undef :test_POST_sends_files if ENV['CI']
