c65d05896932bde58bde33eed7d3d52782a9deac
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-05-29 01:37:19 +0200

    fix Retry middleware on 1.8

--- lib/faraday/request/retry.rb
+++ lib/faraday/request/retry.rb
@@ -1,5 +1,26 @@
 module Faraday
+  # Catches exceptions and retries each request a limited number of times.
+  #
+  # By default, it retries 2 times and handles only timeout exceptions. It can
+  # be configured with an arbitrary number of retries, a list of exceptions to
+  # handle an a retry interval.
+  #
+  # Examples
+  #
+  #   Faraday.new do |conn|
+  #     conn.request :retry, max: 2, interval: 0.05,
+  #                          exceptions: [CustomException, 'Timeout::Error']
+  #     conn.adapter ...
+  #   end
   class Request::Retry < Faraday::Middleware
+    # Public: Initialize middleware
+    #
+    # Options:
+    # max        - Maximum number of retries (default: 2).
+    # interval   - Pause in seconds between retries (default: 0).
+    # exceptions - The list of exceptions to handle. Exceptions can be
+    #              given as Class, Module, or String. (default:
+    #              [Errno::ETIMEDOUT, Timeout::Error, Error::TimeoutError])
     def initialize(app, options = {})
       super(app)
       @retries, options = options, {} if options.is_a? Integer
@@ -8,17 +29,7 @@ module Faraday
       to_handle  = options.fetch(:exceptions) {
                      [Errno::ETIMEDOUT, 'Timeout::Error', Error::TimeoutError]
                    }
-      @errmatch  = ExceptionMatcher.new Array(to_handle)
-    end
-
-    class ExceptionMatcher < Struct.new(:exceptions)
-      def ===(error)
-        exceptions.any? do |ex|
-          if ex.is_a? Module then error.is_a? ex
-          else error.class.to_s == ex.to_s
-          end
-        end
-      end
+      @errmatch  = build_exception_matcher Array(to_handle)
     end
 
     def call(env)
@@ -33,6 +44,24 @@ module Faraday
         end
         raise
       end
+    end
+
+    # Private: construct an exception matcher object.
+    #
+    # An exception matcher for the rescue clause can usually be any object that
+    # responds to `===`, but for Ruby 1.8 it has to be a Class or Module.
+    def build_exception_matcher(exceptions)
+      matcher = Module.new
+      (class << matcher; self; end).class_eval do
+        define_method(:===) do |error|
+          exceptions.any? do |ex|
+            if ex.is_a? Module then error.is_a? ex
+            else error.class.to_s == ex.to_s
+            end
+          end
+        end
+      end
+      matcher
     end
   end
 end
