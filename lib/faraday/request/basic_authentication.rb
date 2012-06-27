dd428bb107d7a5f8c533558966dc02a41ac030e7
Nick Kugaevsky <nick@kugaevsky.ru>
2012-06-27 20:00:18 +0400

    Cleaned up code for basic Authentication. Using strict encoding to throw out gsub.

--- lib/faraday/request/basic_authentication.rb
+++ lib/faraday/request/basic_authentication.rb
@@ -4,8 +4,7 @@ module Faraday
   class Request::BasicAuthentication < Request::Authorization
     # Public
     def self.header(login, pass)
-      value = Base64.encode64([login, pass].join(':'))
-      value.gsub!("\n", '')
+      value = Base64.strict_encode64([login, pass].join(':'))
       super(:Basic, value)
     end
   end
