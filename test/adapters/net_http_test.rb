bcdd1f3edb157bd3c735248f5979aac7b179948b
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-08-12 15:09:52 +0200

    stop using Webmock
    We only used it for one test (now removed) and it doesn't support
    Net::HTTP::Persistent, preventing us from testing that adapter properly.

--- test/adapters/net_http_test.rb
+++ test/adapters/net_http_test.rb
@@ -10,35 +10,6 @@ module Adapters
 
     Integration.apply(self, *behaviors)
 
-    def test_connection_errors_get_wrapped
-      connection = Faraday.new('http://disney.com') do |b|
-        b.adapter :net_http
-      end
-
-      exceptions = [
-        EOFError,
-        Errno::ECONNABORTED,
-        Errno::ECONNREFUSED,
-        Errno::ECONNRESET,
-        Errno::EINVAL,
-        Net::HTTPBadResponse,
-        Net::HTTPHeaderSyntaxError,
-        Net::ProtocolError,
-        SocketError
-      ]
-
-      exceptions << OpenSSL::SSL::SSLError if defined?(OpenSSL)
-
-      exceptions.each do |exception_class|
-        stub_request(:get, 'disney.com/hello').to_raise(exception_class)
-
-        assert_raise(Faraday::Error::ConnectionFailed,
-                     "Failed to wrap #{exception_class} exceptions") do
-          connection.get('/hello')
-        end
-      end
-    end
-
     def test_configure_ssl
       http = Net::HTTP.new 'disney.com', 443
       # this should not raise an error
