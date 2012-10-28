cf0284d56236cc6140af27c5e55413e88b465b53
technoweenie <technoweenie@gmail.com>
2012-10-28 12:33:59 -0600

    spike out a new Builder class

---
+++ lib/faraday/callback_builder.rb
@@ -0,0 +1,115 @@
+module Faraday
+  class CallbackBuilder
+    class StackLocked < RuntimeError; end
+
+    attr_reader :current_adapter, :before, :after
+
+    def initialize(adapter = nil, before = nil, after = nil)
+      @before = Array(before)
+      @after = Array(after)
+      @current_adapter = adapter
+    end
+
+    def request(key, *args, &block)
+      add_before_handler(key, args, block)
+    end
+
+    def response(key, *args, &block)
+      add_after_handler(key, args, block)
+    end
+
+    def adapter(key, *args, &block)
+      set_adapter_handler(key, args, block)
+    end
+
+    def build_response(connection, request)
+      run_request_callbacks(request)
+      response = @current_adapter.call(self, request)
+      run_response_callbacks(response)
+      response
+    end
+
+    def build(options = nil)
+      raise_if_locked
+      unless options && options[:keep]
+        @before.clear
+        @after.clear
+      end
+      yield self if block_given?
+    end
+
+    def lock!
+      @before.freeze
+      @after.freeze
+    end
+
+    def locked?
+      @before.frozen? || @after.frozen?
+    end
+
+  private
+    def run_request_callbacks(request)
+      @before.each { |handler| handler.on_request(self, request) }
+    end
+
+    def run_response_callbacks(response)
+      @after.each { |handler| handler.on_response(self, response) }
+    end
+
+    def add_before_handler(*args)
+      handler = handler_for(*args)
+      @before << handler
+    end
+
+    def add_after_handler(*args)
+      handler = handler_for(*args)
+      @after << handler
+    end
+
+    def set_adapter_handler(*args)
+      handler = handler_for(*args)
+      @current_adapter = handler
+    end
+
+    def handler_for(key, args, block)
+      Handler.new(key, args, block)
+    end
+
+    def raise_if_locked
+      raise StackLocked, "can't modify middleware stack after making a request" if locked?
+    end
+
+    class Handler < Struct.new(:klass, :args, :block)
+      class Invalid < RuntimeError; end
+      class BuilderMismatch < RuntimeError; end
+
+      def on_request(builder, req)
+        inner_handler(builder).on_request(req)
+      end
+
+      def on_response(builder, res)
+        inner_handler(builder).on_response(res)
+      end
+
+      def call(builder, req)
+        inner_handler(builder).call(req)
+      end
+
+      def inner_handler(builder)
+        if @inner_handler
+          if builder != @inner_handler.builder
+            raise BuilderMismatch, "Builders don't match"
+          end
+          @inner_handler
+        else
+          @inner_handler = klass.new(builder, *args, &block)
+        end
+      end
+
+    private
+      def raise_if_builder_mismatch(builder)
+        raise BuilderMismatch, "Builders don't match" if builder != inner_handler.builder
+      end
+    end
+  end
+end
---
+++ test/callback_builder_test.rb
@@ -0,0 +1,108 @@
+require File.expand_path('../helper', __FILE__)
+Faraday.require_lib 'callback_builder'
+
+class CallbackBuilderTest < Faraday::TestCase
+  class Upcaser
+    attr_reader :builder
+    attr_writer :request, :response
+
+    def initialize(builder)
+      @builder = builder
+    end
+
+    def on_request(req)
+      @request = req
+      req.body.upcase!
+    end
+
+    def on_response(res)
+      @response = res
+      res.body.upcase!
+    end
+  end
+
+  class Adapter
+    def initialize(builder)
+      @builder = builder
+    end
+
+    def call(req)
+      Faraday::Response.new :status => 200, :body => 'booya',
+        :response_headers => {"Content-Type" => "text/plain",
+        'X-Body' => req.body}
+    end
+  end
+
+  def test_performs_request
+    builder = build do |b|
+      b.adapter Adapter
+    end
+
+    req = Faraday::Request.new
+    req.body = 'yolo'
+    res = builder.build_response(nil, req)
+    assert_equal 'yolo', res['X-Body']
+    assert_equal 'booya', res.body
+  end
+
+  def test_performs_request_with_callbacks
+    builder = build do |b|
+      b.request Upcaser
+      b.response Upcaser
+      b.adapter Adapter
+    end
+
+    req = Faraday::Request.new
+    req.body = 'yolo'
+    res = builder.build_response(nil, req)
+    assert_equal 'YOLO', res['X-Body']
+    assert_equal 'BOOYA', res.body
+  end
+
+  def test_initializes_with_empty_callbacks
+    builder = self.builder
+    assert_equal [], builder.before
+    assert_equal [], builder.after
+    assert_nil builder.current_adapter
+  end
+
+  def test_adds_request_callback
+    builder = build do |b|
+      b.request Upcaser
+    end
+
+    assert_equal 1, builder.before.size
+    assert_equal [], builder.after
+    assert_nil builder.current_adapter
+  end
+
+  def test_adds_response_callback
+    builder = build do |b|
+      b.response Upcaser
+    end
+
+    assert_equal 1, builder.after.size
+    assert_equal [], builder.before
+    assert_nil builder.current_adapter
+  end
+
+  def test_lock
+    builder = self.builder
+    assert !builder.locked?
+
+    builder.lock!
+
+    assert builder.locked?
+  end
+
+  def builder(*args, &block)
+    Faraday::CallbackBuilder.new(*args, &block)
+  end
+
+  def build(&block)
+    b = builder
+    b.build(&block)
+    b
+  end
+end
+
