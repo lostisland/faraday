edf0fcca18a324e12ab8a9219cf3ed4e6d37ef79
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-02-24 22:36:29 +0100

    fix file uploads with Ruby 2.0 net/http
    Rewrite Faraday::CompositeReadIO because the one inherited from
    multipart-post library doesn't behave well when used with
    IO.copy_stream which net/http uses internally in Ruby 2.0.

--- lib/faraday/upload_io.rb
+++ lib/faraday/upload_io.rb
@@ -8,13 +8,44 @@ rescue LoadError
 end
 
 module Faraday
-  class CompositeReadIO < ::CompositeReadIO
-    attr_reader :length
+  class CompositeReadIO
+    def initialize(*parts)
+      @parts = parts.flatten
+      @ios = @parts.map { |part| part.to_io }
+      @index = 0
+    end
 
-    def initialize(parts)
-      @length = parts.inject(0) { |sum, part| sum + part.length }
-      ios = parts.map{ |part| part.to_io }
-      super(*ios)
+    def length
+      @parts.inject(0) { |sum, part| sum + part.length }
+    end
+
+    def rewind
+      @ios.each { |io| io.rewind }
+      @index = 0
+    end
+
+    def read(length = nil, outbuf = nil)
+      if length.nil? || length.zero?
+        outbuf = outbuf ? outbuf.replace("") : ""
+        @ios.inject(outbuf) { |str, io| str << io.read } if length.nil?
+        outbuf
+      else
+        got_result = false
+        while part = @ios[@index]
+          if result = part.read(length)
+            unless got_result
+              outbuf = outbuf ? outbuf.replace("") : ""
+              got_result = true
+            end
+            result.force_encoding("BINARY") if result.respond_to?(:force_encoding)
+            outbuf << result
+            length -= result.length
+            break if length.zero?
+          end
+          @index += 1
+        end
+        got_result ? outbuf : nil
+      end
     end
   end
 
