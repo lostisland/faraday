f2a54d2e3d8d4732a2eb8bd8ecaa850e08102def
Duke Jones <duke@gopago.com>
2013-04-13 19:38:32 -0700

    Use Base64#strict_encode64 for HTTP Basic authentication.  Extraneous newlines flabbergast IIS.

--- lib/faraday/request/basic_authentication.rb
+++ lib/faraday/request/basic_authentication.rb
@@ -4,7 +4,7 @@ module Faraday
   class Request::BasicAuthentication < Request.load_middleware(:authorization)
     # Public
     def self.header(login, pass)
-      value = Base64.encode64([login, pass].join(':'))
+      value = Base64.strict_encode64([login, pass].join(':'))
       value.gsub!("\n", '')
       super(:Basic, value)
     end
