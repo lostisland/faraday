13f2b2f7f9874356c8ed9b813d1fa5f44ab932af
Timothy Klim <klimtimothy@gmail.com>
2013-05-22 22:55:59 +0400

    Update logger.rb

--- lib/faraday/response/logger.rb
+++ lib/faraday/response/logger.rb
@@ -28,7 +28,7 @@ module Faraday
     private
 
     def dump_headers(headers)
-      headers.map { |k, v| "#{k}: #{v.inspect}" }.join("\n") unless n.empty?
+      headers.map { |k, v| "#{k}: #{v.inspect}" }.join("\n") unless headers.empty?
     end
   end
 end
