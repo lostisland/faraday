71a40603c210a4a511a256df9b50153b614dc9b7
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-06-25 03:58:14 +0200

    simpler FakeSafeBuffer implementation for testing
    references #93

--- test/utils_test.rb
+++ test/utils_test.rb
@@ -2,6 +2,17 @@ require File.expand_path('../helper', __FILE__)
 
 class TestUtils < Faraday::TestCase
 
+  # emulates ActiveSupport::SafeBuffer#gsub
+  FakeSafeBuffer = Struct.new(:string) do
+    def to_s() self end
+    def gsub(regex)
+      string.gsub(regex) {
+        match, = $&, '' =~ /a/
+        yield match
+      }
+    end
+  end
+
   def test_escaping_safe_buffer
     str = FakeSafeBuffer.new('$32,000.00')
     assert_equal '%2432%2C000.00', Faraday::Utils.escape(str)
