cc9986c861f79aeeacfbb4de17ea32d1ecdd9fd3
Prashant <prashant.chaudhari89@gmail.com>
2013-07-02 16:12:57 +0530

    dont raise error for 422

--- lib/faraday/response/raise_error.rb
+++ lib/faraday/response/raise_error.rb
@@ -7,7 +7,7 @@ module Faraday
       when 404
         raise Faraday::Error::ResourceNotFound, response_values(env)
       when 422
-        raise Faraday::Error::UnprocessableEntity, response_values(env)
+        #raise Faraday::Error::UnprocessableEntity, response_values(env)
       when ClientErrorStatuses
         raise Faraday::Error::ClientError, response_values(env)
       end
