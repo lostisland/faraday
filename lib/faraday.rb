2f64974b55359b0ab24949ee1728b257c928ea1b
rick <technoweenie@gmail.com>
2013-11-09 08:54:57 -0700

    bump rc version

--- lib/faraday.rb
+++ lib/faraday.rb
@@ -14,7 +14,7 @@ require 'forwardable'
 #   conn.get '/'
 #
 module Faraday
-  VERSION = "0.9.0.rc5"
+  VERSION = "0.9.0.rc6"
 
   class << self
     # Public: Gets or sets the root path that Faraday is being loaded from.
