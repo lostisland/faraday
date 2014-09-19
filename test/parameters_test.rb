97dd3c437626924c9ab5354033f313d27e75fc74
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-09-19 11:58:56 -0600

    Test passing ActiveSupport::SafeBuffer to param encoders

---
+++ test/parameters_test.rb
@@ -0,0 +1,24 @@
+require File.expand_path("../helper", __FILE__)
+
+class TestParameters < Faraday::TestCase
+  # emulates ActiveSupport::SafeBuffer#gsub
+  FakeSafeBuffer = Struct.new(:string) do
+    def to_s() self end
+    def gsub(regex)
+      string.gsub(regex) {
+        match, = $&, '' =~ /a/
+        yield(match)
+      }
+    end
+  end
+
+  def test_escaping_safe_buffer_nested
+    monies = FakeSafeBuffer.new("$32,000.00")
+    assert_equal "a=%2432%2C000.00", Faraday::Utils.build_nested_query("a" => monies)
+  end
+
+  def test_escaping_safe_buffer_flat
+    monies = FakeSafeBuffer.new("$32,000.00")
+    assert_equal "a=%2432%2C000.00", Faraday::Utils.build_query("a" => monies)
+  end
+end
