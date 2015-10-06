f97f1874e4cd976f564870bb6151fb05e5f4c8b7
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-10-06 18:30:31 +0200

    faraday 0.9.2

--- lib/faraday.rb
+++ lib/faraday.rb
@@ -14,7 +14,7 @@ require 'forwardable'
 #   conn.get '/'
 #
 module Faraday
-  VERSION = "0.9.1"
+  VERSION = "0.9.2"
 
   class << self
     # Public: Gets or sets the root path that Faraday is being loaded from.
