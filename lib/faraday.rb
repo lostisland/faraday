80245b0befa1b7c8077c7898d5d00f5639020da9
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2016-09-30 10:24:39 -0400

    Object#tap no longer required after dropping 1.8.7 (#610)
    Since Faraday only supports Ruby 1.9+, `Object#tap` is a core method and
    this monkey patch is no longer needed.

--- lib/faraday.rb
+++ lib/faraday.rb
@@ -239,25 +239,3 @@ module Faraday
     require_lib 'autoload'
   end
 end
-
-# not pulling in active-support JUST for this method.  And I love this method.
-class Object
-  # The primary purpose of this method is to "tap into" a method chain,
-  # in order to perform operations on intermediate results within the chain.
-  #
-  # Examples
-  #
-  #   (1..10).tap { |x| puts "original: #{x.inspect}" }.to_a.
-  #     tap    { |x| puts "array: #{x.inspect}" }.
-  #     select { |x| x%2 == 0 }.
-  #     tap    { |x| puts "evens: #{x.inspect}" }.
-  #     map    { |x| x*x }.
-  #     tap    { |x| puts "squares: #{x.inspect}" }
-  #
-  # Yields self.
-  # Returns self.
-  def tap
-    yield(self)
-    self
-  end unless Object.respond_to?(:tap)
-end
