0e7089a8fb124bb4d047e9268ba93ea16b52c5d3
Alex Ford <alex@cratr.co>
2017-01-13 04:58:38 -0500

    Default options appear overwritten once set on a Connection instance (#652)
    * Connection initialize always uses ConnectionOptions.from
    
    * Update options.rb
    
    As suggested by @Cohen-Carlisle
    
    Assert that the default is still correct when creating a Connection with a single string `url` argument

--- lib/faraday/connection.rb
+++ lib/faraday/connection.rb
@@ -55,11 +55,11 @@ module Faraday
     #                     :user     - String (optional)
     #                     :password - String (optional)
     def initialize(url = nil, options = nil)
+      options = ConnectionOptions.from(options) unless options and options.is_a?(ConnectionOptions)
+
       if url.is_a?(Hash)
-        options = options ? options.merge(url) : ConnectionOptions.from(url)
+        options = options.merge(url)
         url     = options.url
-      else
-        options = ConnectionOptions.from(options)
       end
 
       @parallel_manager = nil
