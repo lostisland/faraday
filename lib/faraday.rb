bb048c27ab633c51f0b10b7a1f14a1212fc5487d
Brian Carlson <brian.carlson@code42.com>
2017-05-20 22:33:45 -0500

    updated faraday HEAD,GET,DELETE requests to support a body per RFC https://tools.ietf.org/html/rfc7231

--- lib/faraday.rb
+++ lib/faraday.rb
@@ -14,7 +14,7 @@ require 'forwardable'
 #   conn.get '/'
 #
 module Faraday
-  VERSION = "0.12.1"
+  VERSION = "0.13.0"
 
   class << self
     # Public: Gets or sets the root path that Faraday is being loaded from.
