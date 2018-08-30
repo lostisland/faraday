8d218cea573f9083dfd3838e44a33f14ab75bd10
Olle Jonsson <olle.jonsson@gmail.com>
2018-08-30 14:04:02 +0200

    [1.0] Docs for Authorization middleware

--- lib/faraday/request/authorization.rb
+++ lib/faraday/request/authorization.rb
@@ -1,8 +1,11 @@
 module Faraday
+  # Request middleware for the Authorization HTTP header
   class Request::Authorization < Faraday::Middleware
     KEY = "Authorization".freeze unless defined? KEY
 
-    # Public
+    # @param type [String, Symbol]
+    # @param token [String, Symbol, Hash]
+    # @return [String] a header value
     def self.header(type, token)
       case token
       when String, Symbol
@@ -14,7 +17,10 @@ module Faraday
       end
     end
 
-    # Internal
+    # @param type [String]
+    # @param hash [Hash]
+    # @return [String] type followed by comma-separated key=value pairs
+    # @api private
     def self.build_hash(type, hash)
       comma = ", "
       values = []
@@ -24,12 +30,15 @@ module Faraday
       "#{type} #{values * comma}"
     end
 
+    # @param app [Object] Rack app
+    # @param type [String, Symbol] Type of Authorization
+    # @param token [String, Symbol, Hash] Token value for the Authorization
     def initialize(app, type, token)
       @header_value = self.class.header(type, token)
       super(app)
     end
 
-    # Public
+    # @param env [Object] Rack app environment
     def call(env)
       unless env.request_headers[KEY]
         env.request_headers[KEY] = @header_value
