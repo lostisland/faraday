4944486298efac0e91d8f5c33b6e1bf024384986
Olle Jonsson <olle.jonsson@gmail.com>
2018-08-30 14:19:37 +0200

    [1.0] Docs for Basic Authentication, Instrumentation middlewares

--- lib/faraday/request/instrumentation.rb
+++ lib/faraday/request/instrumentation.rb
@@ -1,32 +1,40 @@
 module Faraday
+  # Middleware for instrumenting Requests.
   class Request::Instrumentation < Faraday::Middleware
+    # Options class used in Request::Instrumentation class.
     class Options < Faraday::Options.new(:name, :instrumenter)
+      # @return [String]
       def name
         self[:name] ||= 'request.faraday'
       end
 
+      # @return [Class]
       def instrumenter
         self[:instrumenter] ||= ActiveSupport::Notifications
       end
     end
 
-    # Public: Instruments requests using Active Support.
+    # Instruments requests using Active Support.
     #
     # Measures time spent only for synchronous requests.
     #
-    # Examples
-    #
+    # @example Using ActiveSupport::Notifications to measure time spent for Faraday requests
     #   ActiveSupport::Notifications.subscribe('request.faraday') do |name, starts, ends, _, env|
     #     url = env[:url]
     #     http_method = env[:method].to_s.upcase
     #     duration = ends - starts
     #     $stderr.puts '[%s] %s %s (%.3f s)' % [url.host, http_method, url.request_uri, duration]
     #   end
+    # @param app [Object] Rack app
+    # @param options [nil, Hash] Options hash
+    # @option options [String] :name Name of the instrumenter. Default: "request.faraday"
+    # @option options [Class] :instrumenter Active Support instrumenter class. Default: ActiveSupport::Notifications
     def initialize(app, options = nil)
       super(app)
       @name, @instrumenter = Options.from(options).values_at(:name, :instrumenter)
     end
 
+    # @param env [Object] Rack environment
     def call(env)
       @instrumenter.instrument(@name, env) do
         @app.call(env)
