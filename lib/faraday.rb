81274c93a21e7860dcbb334df19ece81466f1695
Mattia <iMacTia@users.noreply.github.com>
2018-09-19 16:10:22 +0100

    Version bump to 0.15.3

--- lib/faraday.rb
+++ lib/faraday.rb
@@ -14,7 +14,7 @@ require 'forwardable'
 #   conn.get '/'
 #
 module Faraday
-  VERSION = "0.15.2"
+  VERSION = "0.15.3"
 
   class << self
     # Public: Gets or sets the root path that Faraday is being loaded from.
