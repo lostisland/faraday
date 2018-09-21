54b24cdf58e558e2b1a1bf14312a577477bf1f25
iMacTia <giuffrida.mattia@gmail.com>
2018-09-21 17:34:15 +0100

    Converts autorization middleware tests. Moves Faraday::Utils classes (Headers and ParamsHash) into separate files. Converts Faraday::Utils::Headers tests.

---
+++ lib/faraday/utils/params_hash.rb
@@ -0,0 +1,60 @@
+module Faraday
+  module Utils
+    # Hash with stringified keys
+    class ParamsHash < Hash
+      def [](key)
+        super(convert_key(key))
+      end
+
+      def []=(key, value)
+        super(convert_key(key), value)
+      end
+
+      def delete(key)
+        super(convert_key(key))
+      end
+
+      def include?(key)
+        super(convert_key(key))
+      end
+
+      alias_method :has_key?, :include?
+      alias_method :member?, :include?
+      alias_method :key?, :include?
+
+      def update(params)
+        params.each do |key, value|
+          self[key] = value
+        end
+        self
+      end
+      alias_method :merge!, :update
+
+      def merge(params)
+        dup.update(params)
+      end
+
+      def replace(other)
+        clear
+        update(other)
+      end
+
+      def merge_query(query, encoder = nil)
+        if query && !query.empty?
+          update((encoder || Utils.default_params_encoder).decode(query))
+        end
+        self
+      end
+
+      def to_query(encoder = nil)
+        (encoder || Utils.default_params_encoder).encode(self)
+      end
+
+      private
+
+      def convert_key(key)
+        key.to_s
+      end
+    end
+  end
+end
--- test/env_test.rb
+++ test/env_test.rb
@@ -97,75 +97,7 @@ class EnvTest < Faraday::TestCase
   end
 end
 
-class HeadersTest < Faraday::TestCase
-  def setup
-    @headers = Faraday::Utils::Headers.new
-  end
 
-  def test_normalizes_different_capitalizations
-    @headers['Content-Type'] = 'application/json'
-    assert_equal ['Content-Type'], @headers.keys
-    assert_equal 'application/json', @headers['Content-Type']
-    assert_equal 'application/json', @headers['CONTENT-TYPE']
-    assert_equal 'application/json', @headers['content-type']
-    assert @headers.include?('content-type')
-
-    @headers['content-type'] = 'application/xml'
-    assert_equal ['Content-Type'], @headers.keys
-    assert_equal 'application/xml', @headers['Content-Type']
-    assert_equal 'application/xml', @headers['CONTENT-TYPE']
-    assert_equal 'application/xml', @headers['content-type']
-  end
-
-  def test_fetch_key
-    @headers['Content-Type'] = 'application/json'
-    block_called = false
-    assert_equal 'application/json', @headers.fetch('content-type') { block_called = true }
-    assert_equal 'application/json', @headers.fetch('Content-Type')
-    assert_equal 'application/json', @headers.fetch('CONTENT-TYPE')
-    assert_equal 'application/json', @headers.fetch(:content_type)
-    assert_equal false, block_called
-
-    assert_equal 'default', @headers.fetch('invalid', 'default')
-    assert_equal false, @headers.fetch('invalid', false)
-    assert_nil   @headers.fetch('invalid', nil)
-
-    assert_equal 'Invalid key', @headers.fetch('Invalid') { |key| "#{key} key" }
-
-    expected_error = defined?(KeyError) ? KeyError : IndexError
-    assert_raises(expected_error) { @headers.fetch('invalid') }
-  end
-
-  def test_delete_key
-    @headers['Content-Type'] = 'application/json'
-    assert_equal 1, @headers.size
-    assert @headers.include?('content-type')
-    assert_equal 'application/json', @headers.delete('content-type')
-    assert_equal 0, @headers.size
-    assert !@headers.include?('content-type')
-    assert_nil @headers.delete('content-type')
-  end
-
-  def test_parse_response_headers_leaves_http_status_line_out
-    @headers.parse("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n")
-    assert_equal %w(Content-Type), @headers.keys
-  end
-
-  def test_parse_response_headers_parses_lower_cased_header_name_and_value
-    @headers.parse("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n")
-    assert_equal 'text/html', @headers['content-type']
-  end
-
-  def test_parse_response_headers_parses_lower_cased_header_name_and_value_with_colon
-    @headers.parse("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nLocation: http://sushi.com/\r\n\r\n")
-    assert_equal 'http://sushi.com/', @headers['location']
-  end
-
-  def test_parse_response_headers_parses_blank_lines
-    @headers.parse("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/html\r\n\r\n")
-    assert_equal 'text/html', @headers['content-type']
-  end
-end
 
 class ResponseTest < Faraday::TestCase
   def setup
