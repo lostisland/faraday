06f74fcbbbfd33da3148662221e1d80563378374
iMacTia <giuffrida.mattia@gmail.com>
2019-02-19 15:57:46 +0000

    Restores all net_http tests.

--- spec/faraday/adapter/net_http_spec.rb
+++ spec/faraday/adapter/net_http_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Faraday::Adapter::NetHttp do
-  # features :body_on_get, :reason_phrase_parse, :compression, :streaming
+  features :body_on_get, :reason_phrase_parse, :compression, :streaming
 
   it_behaves_like 'an adapter'
 
