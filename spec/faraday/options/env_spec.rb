3d85b50dc3258c5f98f79c058d81fad34cc6403f
iMacTia <giuffrida.mattia@gmail.com>
2019-02-20 14:53:59 +0000

    Improves syntax in `env_spec`.

--- spec/faraday/options/env_spec.rb
+++ spec/faraday/options/env_spec.rb
@@ -1,20 +1,22 @@
 RSpec.describe Faraday::Env do
+  subject(:env) { described_class.new }
+
   it 'allows to access members' do
-    expect(subject.method).to be_nil
-    subject.method = :get
-    expect(subject.method).to eq(:get)
+    expect(env.method).to be_nil
+    env.method = :get
+    expect(env.method).to eq(:get)
   end
 
   it 'allows to access symbol non members' do
-    expect(subject[:custom]).to be_nil
-    subject[:custom] = :boom
-    expect(subject[:custom]).to eq(:boom)
+    expect(env[:custom]).to be_nil
+    env[:custom] = :boom
+    expect(env[:custom]).to eq(:boom)
   end
 
   it 'allows to access string non members' do
-    expect(subject['custom']).to be_nil
-    subject['custom'] = :boom
-    expect(subject['custom']).to eq(:boom)
+    expect(env['custom']).to be_nil
+    env['custom'] = :boom
+    expect(env['custom']).to eq(:boom)
   end
 
   it 'ignores false when fetching' do
@@ -24,42 +26,42 @@ RSpec.describe Faraday::Env do
   end
 
   it 'retains custom members' do
-    subject[:foo] = "custom 1"
-    subject[:bar] = :custom_2
-    env2 = Faraday::Env.from(subject)
+    env[:foo] = "custom 1"
+    env[:bar] = :custom_2
+    env2 = Faraday::Env.from(env)
     env2[:baz] = "custom 3"
 
     expect(env2[:foo]).to eq("custom 1")
     expect(env2[:bar]).to eq(:custom_2)
-    expect(subject[:baz]).to be_nil
+    expect(env[:baz]).to be_nil
   end
 
   describe '#body' do
-    subject { Faraday::Env.from(body: { foo: 'bar' }) }
+    subject(:env) { described_class.from(body: { foo: 'bar' }) }
 
     context 'when response is not finished yet' do
       it 'returns the request body' do
-        expect(subject.body).to eq({ foo: 'bar' })
+        expect(env.body).to eq({ foo: 'bar' })
       end
     end
 
     context 'when response is finished' do
       before do
-        subject.status = 200
-        subject.body = { bar: 'foo' }
-        subject.response = Faraday::Response.new(subject)
+        env.status = 200
+        env.body = { bar: 'foo' }
+        env.response = Faraday::Response.new(env)
       end
 
       it 'returns the response body' do
-        expect(subject.body).to eq({ bar: 'foo' })
+        expect(env.body).to eq({ bar: 'foo' })
       end
 
       it 'allows to access request_body' do
-        expect(subject.request_body).to eq({ foo: 'bar' })
+        expect(env.request_body).to eq({ foo: 'bar' })
       end
 
       it 'allows to access response_body' do
-        expect(subject.response_body).to eq({ bar: 'foo' })
+        expect(env.response_body).to eq({ bar: 'foo' })
       end
     end
   end
