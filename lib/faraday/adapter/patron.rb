25b1055dc03d0f0c254c7021764835b2e8540c84
James Herdman <james.herdman@gmail.com>
2019-02-27 08:57:26 -0500

    chore: RuboCop Layout Rescue Ensure Alignment

--- lib/faraday/adapter/patron.rb
+++ lib/faraday/adapter/patron.rb
@@ -30,8 +30,8 @@ module Faraday
         response = begin
           data = env[:body] ? env[:body].to_s : nil
           session.request(env[:method], env[:url].to_s, env[:request_headers], data: data)
-        rescue Errno::ECONNREFUSED, ::Patron::ConnectionFailed
-          raise Faraday::ConnectionFailed, $!
+                   rescue Errno::ECONNREFUSED, ::Patron::ConnectionFailed
+                     raise Faraday::ConnectionFailed, $!
         end
 
         if (req = env[:request]).stream_response?
