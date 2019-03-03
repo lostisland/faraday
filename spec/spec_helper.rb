f1d0b3933d7cb3f89e1f68669872b4d83d3f6242
htwroclau <35439320+htwroclau@users.noreply.github.com>
2019-03-03 17:58:00 +0900

    chore: Fix RuboCop Style/EmptyMethod (#912)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -120,11 +120,9 @@ end
 
 # Extends RSpec DocumentationFormatter to hide skipped tests.
 module FormatterOverrides
-  def example_pending(_arg)
-  end
+  def example_pending(_arg); end
 
-  def dump_pending(_arg)
-  end
+  def dump_pending(_arg); end
 
   RSpec::Core::Formatters::DocumentationFormatter.prepend self
 end
