f8ab31a264376a335bfbfa728256dfa874f303f7
Geoff Hubbard <ghub@instihost.com>
2019-03-04 22:07:43 +0000

    fix typos

--- spec/faraday_spec.rb
+++ spec/faraday_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe Faraday do
   end
 
   context 'proxies to default_connection' do
-    let(:mock_conection) { double('Connection') }
+    let(:mock_connection) { double('Connection') }
     before do
-      Faraday.default_connection = mock_conection
+      Faraday.default_connection = mock_connection
     end
 
     it 'proxies methods that exist on the default_connection' do
-      expect(mock_conection).to receive(:this_should_be_proxied)
+      expect(mock_connection).to receive(:this_should_be_proxied)
 
       Faraday.this_should_be_proxied
     end
 
-    it 'uses method_missing on Farady if there is no proxyable method' do
+    it 'uses method_missing on Faraday if there is no proxyable method' do
       expect { Faraday.this_method_does_not_exist }.to raise_error(
         NoMethodError,
         "undefined method `this_method_does_not_exist' for Faraday:Module"
@@ -25,7 +25,7 @@ RSpec.describe Faraday do
     end
 
     it 'proxied methods can be accessed' do
-      allow(mock_conection).to receive(:this_should_be_proxied)
+      allow(mock_connection).to receive(:this_should_be_proxied)
 
       expect(Faraday.method(:this_should_be_proxied)).to be_a(Method)
     end
