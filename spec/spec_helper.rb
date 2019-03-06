b9ea1d92ce11710e0f70b56037f434afd2a094e4
Geoff Hubbard <Insti@users.noreply.github.com>
2019-03-06 09:46:51 +0100

    Accurate test code coverage percentage (#934)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,13 @@ SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCo
 
 SimpleCov.start do
   add_filter '/spec/'
-  minimum_coverage 90
-  minimum_coverage_by_file 70
+  minimum_coverage 84
+  minimum_coverage_by_file 26
 end
+
+# Ensure all /lib files are loaded
+# so they will be included in the test coverage report.
+Dir['./lib/**/*.rb'].each { |file| require file }
 
 require 'faraday'
 require 'pry'
