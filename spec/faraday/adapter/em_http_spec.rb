abade0d4530b858a76ae5ac73ed8d53227fb81d1
rick olson <technoweenie@gmail.com>
2019-09-19 15:32:22 -0600

    test current EMHttp timeout config

--- spec/faraday/adapter/em_http_spec.rb
+++ spec/faraday/adapter/em_http_spec.rb
@@ -13,4 +13,32 @@ RSpec.describe Faraday::Adapter::EMHttp do
 
     expect(req.connopts.inactivity_timeout).to eq(20)
   end
+
+  context 'Options' do
+    let(:request) { Faraday::RequestOptions.new }
+    let(:env) { { request: request } }
+    let(:adapter) {
+      Object.new.tap do |o|
+        class << o
+          include Faraday::Adapter::EMHttp::Options
+        end
+      end
+    }
+    let(:options) { {} }
+
+    it 'configures timeout' do
+      request.timeout = 5
+      adapter.configure_timeout(options, env)
+      expect(options[:inactivity_timeout]).to eq(5)
+      expect(options[:connect_timeout]).to eq(5)
+    end
+
+    it 'configures timeout and open_timeout' do
+      request.timeout = 5
+      request.open_timeout = 1
+      adapter.configure_timeout(options, env)
+      expect(options[:inactivity_timeout]).to eq(5)
+      expect(options[:connect_timeout]).to eq(1)
+    end
+  end
 end
