620c4d12711bcd882ea93fe9e2e611f9b8e3d621
rick olson <technoweenie@gmail.com>
2019-10-17 19:29:28 -0600

    remove unnecessary #tap

--- lib/faraday/deprecate.rb
+++ lib/faraday/deprecate.rb
@@ -8,8 +8,8 @@ module Faraday
   # @see Faraday::Deprecate
   module DeprecatedClass
     def self.proxy_class(new_klass)
-      Class.new(new_klass).tap do |k|
-        class << k
+      Class.new(new_klass) do
+        class << self
           extend Faraday::Deprecate
           # Make this more human readable than #<Class:Faraday::ClientError>
           klass_name = superclass.to_s[/^#<Class:([\w:]+)>$/, 1]
