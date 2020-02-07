e6de567e36b4bdbb03e68005e24a54d3ea1ac040
Olle Jonsson <olle.jonsson@gmail.com>
2020-02-07 16:15:52 +0100

    Re-enable rack tests

--- spec/faraday/adapter/rack_spec.rb
+++ spec/faraday/adapter/rack_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe Faraday::Adapter::Rack do
   features :request_body_on_query_methods, :trace_method,
            :skip_response_body_on_head
 
-  it_behaves_like 'an adapter', adapter_options: WebmockRackApp.new, skip: true
+  it_behaves_like 'an adapter', adapter_options: WebmockRackApp.new
 end
