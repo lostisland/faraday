3fee0151b78abebef9b7cc620de031ae56961412
Matt <iMacTia@users.noreply.github.com>
2021-08-15 09:28:49 +0100

    Drop deprecated auth helpers from Connection. (#1308)
    Merge Authentication middleware.
    Add possibility of passing a proc.
    Update documentation.
    
    (cherry picked from commit fe600c72adc79180ad7e545191edd5dd2fe61990)

--- lib/faraday/request/authorization.rb
+++ lib/faraday/request/authorization.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'base64'
+
 module Faraday
   class Request
     # Request middleware for the Authorization HTTP header
@@ -8,47 +10,46 @@ module Faraday
         KEY = 'Authorization'
       end
 
-      # @param type [String, Symbol]
-      # @param token [String, Symbol, Hash]
-      # @return [String] a header value
-      def self.header(type, token)
-        case token
-        when String, Symbol
-          "#{type} #{token}"
-        when Hash
-          build_hash(type.to_s, token)
-        else
-          raise ArgumentError,
-                "Can't build an Authorization #{type}" \
-                  "header from #{token.inspect}"
-        end
-      end
-
-      # @param type [String]
-      # @param hash [Hash]
-      # @return [String] type followed by comma-separated key=value pairs
-      # @api private
-      def self.build_hash(type, hash)
-        comma = ', '
-        values = []
-        hash.each do |key, value|
-          values << "#{key}=#{value.to_s.inspect}"
-        end
-        "#{type} #{values * comma}"
-      end
-
       # @param app [#call]
       # @param type [String, Symbol] Type of Authorization
-      # @param token [String, Symbol, Hash] Token value for the Authorization
-      def initialize(app, type, token)
-        @header_value = self.class.header(type, token)
+      # @param params [Array<String, Proc>] parameters to build the Authorization header.
+      #   If the type is `:basic`, then these can be a login and password pair.
+      #   Otherwise, a single value is expected that will be appended after the type.
+      #   This value can be a proc, in which case it will be invoked on each request.
+      def initialize(app, type, *params)
+        @type = type
+        @params = params
         super(app)
       end
 
       # @param env [Faraday::Env]
-      def call(env)
-        env.request_headers[KEY] = @header_value unless env.request_headers[KEY]
-        @app.call(env)
+      def on_request(env)
+        return if env.request_headers[KEY]
+
+        env.request_headers[KEY] = header_from(@type, *@params)
+      end
+
+      private
+
+      # @param type [String, Symbol]
+      # @param params [Array]
+      # @return [String] a header value
+      def header_from(type, *params)
+        if type.to_s.casecmp('basic').zero? && params.size == 2
+          basic_header_from(*params)
+        elsif params.size != 1
+          raise ArgumentError, "Unexpected params received (got #{params.size} instead of 1)"
+        else
+          value = params.first
+          value = value.call if value.is_a?(Proc)
+          "#{type} #{value}"
+        end
+      end
+
+      def basic_header_from(login, pass)
+        value = Base64.encode64("#{login}:#{pass}")
+        value.delete!("\n")
+        "Basic #{value}"
       end
     end
   end
