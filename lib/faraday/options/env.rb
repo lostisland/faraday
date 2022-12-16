8e55f7e3ddf4c206f45a11902a14bafee04a248b
Henry Yang <19463606+Yu-Chieh-Henry-Yang@users.noreply.github.com>
2022-12-16 11:37:56 +1030

    Emphasize that timeout represents open plus read

--- lib/faraday/options/env.rb
+++ lib/faraday/options/env.rb
@@ -15,7 +15,7 @@ module Faraday
   #   @return [Hash] options for configuring the request.
   #   Options for configuring the request.
   #
-  #   - `:timeout`      - timeout for both the connection-opening phase and the reading phase after it (Integer in seconds)
+  #   - `:timeout`      - total timeout for both the connection-opening phase and the reading phase after it added together (Integer in seconds)
   #   - `:open_timeout` - timeout for only the connection-opening phase (Integer in seconds)
   #   - `:on_data`      - Proc for streaming
   #   - `:proxy`        - Hash of proxy options
